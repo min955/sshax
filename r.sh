2e85a9eb911a7fa0690c22d77f5da0073619fea3
min955 <141538346+min955@users.noreply.github.com>
2023-08-06 11:04:38 +0700

    Create r.sh

---
+++ r.sh
@@ -0,0 +1,2 @@
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
